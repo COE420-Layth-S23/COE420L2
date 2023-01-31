0fe6a25bcac645f224f8ccde376ba779d9f0e9ce
Bingxuan Li <b00088619@aus.edu>
2023-01-31 17:53:29 +0400

    Feature B added

--- COE420L2.java
+++ COE420L2.java
@@ -4,6 +4,13 @@ public class COE420L2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello World");
+		
+		featureB();
+	}
+	
+	public static void featureB()
+	{
+		System.out.println("This is feature B");
 	}
 
 }
